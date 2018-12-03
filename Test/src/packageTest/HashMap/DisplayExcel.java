9982fec8fbc0ce6c564a2b4023d3f807db38e366
palhot007@gmail.com <dharmpal.kumar@qualtechedge.com>
2018-12-03 13:02:40 +0530

    Disp Excel Data

---
+++ Test/src/packageTest/HashMap/DisplayExcel.java
@@ -0,0 +1,25 @@
+package packageTest.HashMap;
+
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class DisplayExcel {
+
+			@SuppressWarnings("resource")
+		   public static void main(String[] args) throws Exception
+		   {
+		      //Build reader instance
+		      CSVReader reader = new CSVReader(new FileReader("D:/05NOV2015/releaseTracker.xls"), ',', '"', 1);
+		       
+		      //Read all rows at once
+		      List<String[]> allRows = reader.readAll();
+		       
+		      //Read CSV line by line and use the string array as you want
+		     for(String[] row : allRows){
+		        System.out.println(Arrays.toString(row));
+		     }
+		   }
+	}
---
+++ Test/src/packageTest/HashMap/MapStream.java
@@ -0,0 +1,14 @@
+package packageTest.HashMap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapStream {
+
+	public static void main(String[] args) {
+		
+		List<String> cs = Arrays.asList("agent", "manager", "admin");
+		
+		
+	}
+}
